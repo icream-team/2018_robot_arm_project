3fcd54a4cd766c75397cabbfe586d271c0556abe
ITJEONG <derbana1027@gmail.com>
2018-06-19 18:31:40 +0900

    최종본

--- 2D/SpaceShoot/main.lua
+++
@@ -1,9 +0,0 @@
-local composer = require "composer"
-
-local options =
-{
-    effect = "fade",
-    tiem = 500,
-}
-
-composer.gotoScene("lobby", options)
--- ARDUINO PART/Final/Final.ino
+++ ARDUINO PART/Final/Final.ino
@@ -1,14 +1,40 @@
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
 #include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#include "MPU6050.h"
 
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
 
-#define OUTPUT_READABLE_QUATERNION
-#define OUTPUT_READABLE_YAWPITCHROLL
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
 
+/* =========================================================================
+   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the Arduino's
+   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
+   digital I/O pin 2.
+ * ========================================================================= */
+
+/* =========================================================================
+   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
+   when using Serial.write(buf, len). The Teapot output uses this method.
+   The solution requires a modification to the Arduino USBAPI.h file, which
+   is fortunately simple, but annoying. This will be fixed in the next IDE
+   release. For more info, see these links:
+
+   http://arduino.cc/forum/index.php/topic,109987.0.html
+   http://code.google.com/p/arduino/issues/detail?id=958
+ * ========================================================================= */
 #define vibe1 A2
 #define vibe2 A3
 #define MAGNET1 7
@@ -17,48 +43,71 @@
 #define MAGNET4 5
 #define MAGNET5 6
 
-MPU6050 mpu;
 
-bool dmpReady = false;
-uint8_t mpuIntStatus;
-uint8_t devStatus;
-uint16_t packetSize;
-uint16_t fifoCount;
-uint8_t fifoBuffer[64];
+// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+// quaternion components in a [w, x, y, z] format (not best for parsing
+// on a remote host such as Processing or something though)
+#define OUTPUT_READABLE_QUATERNION      //실제 w x y z를 보기위한 것.
 
-Quaternion q;
-VectorInt16 aa;
-VectorInt16 aaReal;
-VectorInt16 aaWorld;
-VectorFloat gravity;
 
-float euler[3];
-float ypr[3];
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL      //yaw pitch roll을 보고싶을때
 
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful   //DMP를 입력하면 시작.
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 
-volatile bool mpuInterrupt = false;
 
-void dmpDataReady()
-{
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
     mpuInterrupt = true;
 }
 
-int vibrationcommand; // 진동 커맨드
-boolean finish = false; // 진동이 끝났는가
-unsigned long preTime = 0; // 이전 기록 시간
-int continuationTime = 500; // 진동 지속 시간
 
-void setup()
-{
-    pinMode( vibe1, OUTPUT );
-    pinMode( vibe2, OUTPUT );
-    pinMode( MAGNET1, INPUT );
-    pinMode( MAGNET2, INPUT );
-    pinMode( MAGNET3, INPUT );
-    pinMode( MAGNET4, INPUT );
-    pinMode( MAGNET5, INPUT );
-    
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+  pinMode(13 , OUTPUT ); //자이로
+  pinMode(12 , OUTPUT );
+  pinMode( vibe1 , OUTPUT );
+  pinMode( vibe2 , OUTPUT);
+  pinMode( MAGNET1 , INPUT);
+  pinMode( MAGNET2 , INPUT);
+  pinMode( MAGNET3 , INPUT);
+  pinMode( MAGNET4 , INPUT);
+  pinMode( MAGNET5 , INPUT);
+
+    // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
         TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
@@ -66,45 +115,64 @@ void setup()
         Fastwire::setup(400, true);
     #endif
 
-    Serial.begin(9600);
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(9600);  //원래 115200
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
-    while(!Serial);
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
+    // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
 
-    Serial.println( F("Initializing I2C device..."));
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
 
+    // verify connection
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
+    // wait for ready
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while(Serial.available() && Serial.read());
-    while(!Serial.available());
-    while(Serial.available()&&Serial.read());
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
 
+    // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
-    mpu.setXGyroOffset(0);
-    mpu.setYGyroOffset(0);
-    mpu.setZGyroOffset(0);
-    mpu.setZAccelOffset(0);
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(0);    //220
+    mpu.setYGyroOffset(0);     //76
+    mpu.setZGyroOffset(0);     //-85
+    mpu.setZAccelOffset(0); // 1788 factory default for my test chip
 
-    if(devStatus==0)
-    {
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
         Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
+        // enable Arduino interrupt detection
         Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
         attachInterrupt(0, dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
         Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
+        // get expected DMP packet size for later comparison
         packetSize = mpu.dmpGetFIFOPacketSize();
-    }
-    else
-    {
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
         Serial.print(F("DMP Initialization failed (code "));
         Serial.print(devStatus);
         Serial.println(F(")"));
@@ -112,32 +180,57 @@ void setup()
 
 }
 
-
-
+int state_13=0;
+int state_12=0;
+char vibrationcommand;
+boolean finish=false;
+unsigned long preTime = 0;
 void timecheck()
 {
-    unsigned long curTime = millis();
-
-    if( curTime > preTime + continuationTime )
-    {
-        analogWrite( vibe1, 0 );
-        analogWrite( vibe2, 0 );
-
-        finish = true;
-    }
+unsigned long curTime = millis(); // 현재 시각은?
+// (현재시각 > 이전시각+1초)
+if(curTime > preTime+1000) { // 1초 지났니?
+finish=true;
+curTime=0;
 }
+}
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
 
-void send_pos_and_ang()
-{
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-    {
+void loop() {
+
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
         // reset so we can continue cleanly
         mpu.resetFIFO();
         Serial.println(F("FIFO overflow!"));
 
-        // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02)
-    {
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
         // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
 
@@ -149,98 +242,98 @@ void send_pos_and_ang()
         fifoCount -= packetSize;
 
         #ifdef OUTPUT_READABLE_QUATERNION
-        // display quaternion values in easy matrix form: w x y z
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        Serial.print("POX");
-        Serial.println(q.w);
+            // display quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            Serial.print("POX");
+            Serial.println(q.w);
 
-        Serial.print("POY");
-        Serial.println(q.x);
+            Serial.print("POY");
+            Serial.println(q.x);
 
-        Serial.print("POZ");
-        Serial.println(q.z);
-
-        Serial.flush();
+            Serial.print("POZ");
+            Serial.println(q.z);
+            /*
+            Serial.print("quat\t");
+            Serial.print(q.w);
+            Serial.print("\t");
+            Serial.print(q.x);
+            Serial.print("\t");
+            Serial.print(q.y);
+            Serial.print("\t");
+            Serial.println(q.z);
+            */
         #endif
 
         #ifdef OUTPUT_READABLE_YAWPITCHROLL
-        // display Euler angles in degrees
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        Serial.print("ANX");
-        Serial.println( int( ypr[0] * 180 / M_PI ) );
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ANX");
+            Serial.println( int( ypr[0] * 180 / M_PI ) );
 
-        Serial.print("ANY");
-        Serial.println( int( ypr[1] * 180 / M_PI ) );
+            Serial.print("ANY");
+            Serial.println( int( ypr[1] * 180 / M_PI ) );
 
-        Serial.print("ANZ");
-        Serial.println( int( ypr[2] * 180 / M_PI ) );
-
-        Serial.flush();
-
+            Serial.print("ANZ");
+            Serial.println( int( ypr[2] * 180 / M_PI ) );
+            /*
+            Serial.print("yawpitchRoll\t");
+            Serial.print(int(ypr[0] * 180/M_PI));
+            Serial.print("\t");
+            Serial.print(int(ypr[1] * 180/M_PI));
+            Serial.print("\t");
+            Serial.println(int(ypr[2] * 180/M_PI));
+            */
         #endif
+        // blink LED to indicate activity
+        blinkState = !blinkState;
     }
-}
-
-void convension_serial_message( char * message )
+if(!Serial.available())
 {
-    if ( message[0] == "V" && message[1] == "I" )
-    {
-        vibration_signal( (int)message[2] );
-    }
+  unsigned long curTime = millis(); // 현재 시각은?
+// (현재시각 > 이전시각+1초)
+
+if(curTime > preTime+1000) { // 1초 지났니?
+preTime = curTime;
+    analogWrite( vibe1, 0);
+    analogWrite( vibe2, 0);
+  }
 }
-
-void vibration_signal( int type )
-{
-    preTime = millis();
-    finish = false;
-
-    if( type == 1 )
-    {
-        analogWrite( vibe1, 100 );
-        analogWrite( vibe2, 100 );
-        continuationTime = 250;
-    }
-    else if( type == 2 )
-    {
-        analogWrite( vibe1, 300 );
-        analogWrite( vibe2, 300 );
-        continuationTime = 150;
-    }
-    else if( type == 3 )
-    {
-        analogWrite( vibe1, 200 );
-        analogWrite( vibe2, 200 );
-        continuationTime = 500;
-    }
-}
-
-void loop()
-{
+      if(Serial.available()>0)
+ {
+      vibrationcommand=Serial.read();
+    if(vibrationcommand!='1'||vibrationcommand!='2')
+        {
+           analogWrite( vibe1, 0);
+            analogWrite( vibe2, 0);
+         }
+    if(vibrationcommand='1')
+     {
+     analogWrite( vibe1, 100);
+      analogWrite( vibe2, 100);
+    //총쏘기
     timecheck();
-
-    if (!dmpReady) return;
-
-    send_pos_and_ang();
-
-    mpuIntStatus = mpu.getIntStatus();
-
-    fifoCount = mpu.getFIFOCount();
-
-    if(Serial.available())
-    {
-      char temp_value[10];
-      char temp = 'a';
-      for( int i = 0; i < 10; i++ )
-      {
-        temp  = Serial.read();
-
-        if( !temp ) break;
-        temp_value[i] = Serial.read();
+       if(finish)
+       {
+       analogWrite( vibe1, 0);
+        analogWrite( vibe2, 0);
+        finish=false;
       }
-
-        convension_serial_message( temp_value );
-    }
-}
-
+     }
+    if(vibrationcommand='2')
+    {
+    //특수공격
+    //센 진동
+      analogWrite( vibe1, 300);
+      analogWrite( vibe2, 300);
+       timecheck();
+     if(finish)
+        {
+          analogWrite( vibe1, 0);
+         analogWrite( vibe2, 0);
+         finish=false;
+       }
+     }
+  }
+ }
